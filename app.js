20258d452222640d5d405c9b5be43d5f5e0ec9a6
Khakaroto <rafaelborges.br@gmail.com>
2024-11-24 13:32:39 -0300

    Voltando para estado do Paulo

--- app.js
+++ app.js
@@ -2,6 +2,3 @@ console.log("Rodando o sistema de gerenciamento de joggos")
 console.log("Aplicação está se conectando ao banco de dados")
 // mais um console logg de teste
 console.log("O Paulo fez esse commit sem você ver a aula!")
-console.log("Enviando para o GitHub")
-
-
