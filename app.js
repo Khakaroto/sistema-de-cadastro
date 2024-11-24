06181bc4a856123c3bea5645a29528357893ef7b
Khakaroto <rafaelborges.br@gmail.com>
2024-11-24 13:17:18 -0300

    Console.log indicando modificacoes do Paulo

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de joggos")
 console.log("Aplicação está se conectando ao banco de dados")
+// mais um console logg de teste
+console.log("O Paulo fez esse commit sem você ver a aula!")
